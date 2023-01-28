abe4fb6478a0e90fa9f3b0abad87509cdd7d5c4a
kalibetre <kalbetre@icloud.com>
2023-01-28 11:05:09 +0300

    fetch places

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -12,7 +12,7 @@ $('document').ready(() => {
         $('.amenities h4').text(text);
     });
 
-    $.get('http://127.0.0.1:5001/api/v1/status/', (data, status) => {
+    $.get('http://0.0.0.0:5001/api/v1/status/', (data, status) => {
         console.log(data);
         if (data['status'] === 'OK') $('#api_status').addClass('available');
         else $('#api_status').removeClass('available');
